ddcfc5cd3b775c13f4826355389ae645098338e3
bing-Guo <bindaguo@gmail.com>
2017-04-04 15:22:58 +0800

    Add files via upload

---
+++ Beautiful_Soup.py
@@ -0,0 +1,30 @@
+
+# coding: utf-8
+
+# In[82]:
+
+import requests
+from bs4 import BeautifulSoup
+import time
+
+
+# In[84]:
+
+tStart = time.time()
+for i in range(5):
+    print("page: "+str((i+1)))
+    url = "https://tw.search.bid.yahoo.com/search/auction/product"
+    url_params = {"p":"辦公椅", "qt":"product", "kw":"辦公椅", "cid":0, "clv":0, "acu":0, "pg":(i+1)}
+    res = requests.get(url, params = url_params)
+    soup = BeautifulSoup(res.text, "html5lib")
+    
+    for product in soup.select(".att-item"):
+        name = product.select(".srp-pdtitle a")[0].text
+        href = product.select(".srp-pdtitle")[0].find("a").attrs['href']
+        price = product.select(".srp-pdprice em")[0].text
+        print(name+"\n"+href+"\n"+price)
+        print("\n")
+
+tEnd = time.time()
+print("執行時間 :"+str(tEnd - tStart))
+
